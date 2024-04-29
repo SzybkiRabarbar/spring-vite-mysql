b97fc6c7e65cecf65d14497e755b2de12f455ecc
SzybkiRabarbar <maksjoni36@gmail.com>
2024-04-29 23:54:54 +0200

    Add LoadProductTableData util

--- src/main/java/server/demo/models/Product.java
+++ src/main/java/server/demo/models/Product.java
@@ -31,7 +31,7 @@ public class Product {
     @Column
     private String brand;
 
-    @Column
+    @Column(columnDefinition = "LONGTEXT")
     private String description;
 
     @Column
@@ -40,8 +40,123 @@ public class Product {
     @Column
     private Integer reviews;
 
-    @Column
+    @Column(columnDefinition = "LONGTEXT")
     private String images;
 
-    // getters and setters
+    public Product(String productName, String productId, Integer listingPrice,
+            Integer salePrice, Integer discount, String brand,
+            String description, Double rating, Integer reviews, String images) {
+        this.productName = productName;
+        this.productId = productId;
+        this.listingPrice = listingPrice;
+        this.salePrice = salePrice;
+        this.discount = discount;
+        this.brand = brand;
+        this.description = description;
+        this.rating = rating;
+        this.reviews = reviews;
+        this.images = images;
+    }
+
+    public Product(String[] data) {
+        this.productName = data[0];
+        this.productId = data[1];
+        this.listingPrice = Integer.parseInt(data[2]);
+        this.salePrice = Integer.parseInt(data[3]);
+        this.discount = Integer.parseInt(data[4]);
+        this.brand = data[5];
+        this.description = data[6];
+        this.rating = Double.parseDouble(data[7]);
+        this.reviews = Integer.parseInt(data[8]);
+        this.images = data[9];
+    }
+
+    // * GETTERS * SETTERS *
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getListingPrice() {
+        return listingPrice;
+    }
+
+    public void setListingPrice(Integer listingPrice) {
+        this.listingPrice = listingPrice;
+    }
+
+    public Integer getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(Integer salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Integer getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Integer reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
 }
---
+++ src/main/java/server/demo/utils/LoadProductTableData.java
@@ -0,0 +1,54 @@
+package server.demo.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import server.demo.models.Product;
+import server.demo.models.ProductRepository;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@Component
+public class LoadProductTableData implements CommandLineRunner {
+
+    private final ProductRepository repository;
+
+    @Autowired
+    public LoadProductTableData(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override  // Run then table is empty
+    public void run(String... strings) throws Exception {
+        if (repository.count() != 0) {
+            return;
+        }
+
+        String csvFile = Paths.get("src", "main", "resources", "static", "nike_shoes_sales.csv").toString();
+
+        try (Reader in = new FileReader(csvFile)) {
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                String[] data = new String[record.size()];
+                for (int i = 0; i < record.size(); i++) {
+                    data[i] = record.get(i);
+                    System.err.println(data[i]);
+                }
+
+                Product product = new Product(data);
+
+                repository.save(product);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
